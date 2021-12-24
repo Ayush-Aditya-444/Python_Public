6cf8ee8ad734cbc6157e79bc3775393ef0efef3c
Ayush-Aditya-444 <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-24 20:12:43 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,41 @@
+print("There are 5 Types Of Data Types In Python")
+print("1.Numbers:-")
+Value1=10
+Value2=9.5
+Value3=1+3j
+print(Value1)
+print(type(Value1))
+print(Value2)
+print(type(Value2))
+print(Value3)
+print(type(Value3))
+print("2.Squence Type:-")
+print("(i)String:-")
+Name="Ayush"
+print(Name)
+print(type(Name))
+print("(ii)Lists:-")
+list1=[1,"Ayush"]
+print(type(list1))
+print(list1)
+print(list1[:1])
+print(list1*3)
+print("3.Boolean:-")
+My_Name_Ayush=True
+print(My_Name_Ayush)
+print(type(My_Name_Ayush))
+print("4.Dictionary:-")
+d={1:"Ayush", 2:"Ragini", 3:"Anil", 4:"Manju"}
+print(type(d))
+print(d)
+print("3rd Name is:- "+d[3])
+print(d.keys())
+print(d.values())
+print("5.Sets;-")
+set1 = {'James', 2, 3,'Python'}
+print(type(set1))
+print(set1)  
+set1.add(10)  
+print(set1)  
+set1.remove(2)  
+print(set1) 
