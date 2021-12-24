0377734d36085836d42c9c45f7f62dddb5eabef5
Ayush-Aditya-444 <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-24 20:12:24 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,16 @@
+print("Example Of Variables:-")
+Price=10 #Integer
+Rating=4.9 #Float
+Name="Ayush" #String
+is_published=True #Boolean
+print(Price)
+print(Rating)
+print(Name)
+print(is_published)
+print("Question On Variables:-")
+Patient_Name="John Smith"
+Age=20
+is_new_Patient=True
+print(Patient_Name)
+print(Age)
+print(is_new_Patient)
