fe6650c19247e41926358a34bb76302c0caff04f
Ayush-Aditya-444 <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-24 21:51:46 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,9 @@
+String1='Hi, My Name is "Ayush Aditya"'
+String2="Hi, I am Anisha's Father Ayush Aditya"
+String3='''Hi, I am Anisha's Father "Ayush Aditya"'''
+print(String1)
+print(String2)
+print(String3)
+print(String1[0])
+print(String2[0:3])
+print(String3[-5])
