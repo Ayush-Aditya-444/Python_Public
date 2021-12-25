0816ca5520871b2d1b6df364e73a928a4d7a3fb8
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-25 23:37:22 +0530

    Add files via upload

---
+++ For Loop.py
@@ -0,0 +1,19 @@
+print("For Loop In Python")
+print("Example 1:-")
+for a in "Python":
+    print(a)
+print("Example 2:-")
+for b in ['Ayush', 'Ragini', 'Anil', 'Manju']:
+    print(b)
+print("Example 3:-")
+for c in [1,2,3,4,5]:
+    print(c)
+print("Example 4:-")
+for d in range(10):
+    print(d)
+print("Example 5:-")
+for e in range(5,20):
+    print(e)
+print("Example 6:-")
+for f in range(5,20,2):
+    print(f)
