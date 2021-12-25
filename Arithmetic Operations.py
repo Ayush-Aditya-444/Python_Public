097eed6b1c9c824e833af97b064ac80a46fbc5f7
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-25 18:38:58 +0530

    Add files via upload

---
+++ Arithmetic Operations.py
@@ -0,0 +1,53 @@
+print("Arithmetic Operations In Python:-")
+x=10
+y=15
+print(x)
+print(y)
+print("Addition:-")
+a=x+y
+print(a)
+print("Subtraction:-")
+b=x-y
+print(b)
+print("Division:-")
+c=x/y
+print(c)
+print("Multiplication:-")
+d=x*y
+print(d)
+print("Remainder:-")
+e=x%y
+print(e)
+print("Exponents:-")
+f=x**y #For Exponents
+print(f)
+print("Floor Division:-")
+g=x//y #Gives only Interger Part
+print(g)
+print("X+X:-")
+x+=x
+print(x)
+x=10
+print("X-X:-")
+x-=x
+print(x)
+x=10
+print("X*X:-")
+x*=x
+print(x)
+x=10
+print("X/X (For Reaminder):-")
+x%=x
+print(x)
+x=10
+print("X^10:-")
+x**=x
+print(x)
+x=10
+print("X/X (Only Interger Part):-")
+x//=x
+print(x)
+print("Operator Precedence:-")
+print("Calculate (10+3)**2-3*4/5:-")
+x=(10+3)**2-3*4/5
+print(x)
