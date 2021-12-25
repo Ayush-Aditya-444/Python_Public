05919ce4d9dbd813e79bc63ebb38ac296234ef6e
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-25 19:17:38 +0530

    Add files via upload

---
+++ If Statements.py
@@ -0,0 +1,25 @@
+print("If Statements:-")
+print("Example:-")
+is_hot=False
+is_cold=False
+if is_cold:
+    print("It's A Cold Day")
+    print("Stay Inside Your Home")
+    print("And Make Sure You Waer Warm Colthes")
+elif is_hot:
+    print("It's A Hot Day")
+    print("Stay Inside Your Home")
+    print("And Make Sure You Drink Plenty Of Water")
+else:
+    print("It's A Lovely Day")
+    print("You Can Go Around Where Ever You Want")
+print("Question:-")
+good_credit=False
+if good_credit:
+    x=1000000
+    y=x*(10/100)
+    print(f"Down Payment = Rs.{y}")
+else:
+    x=1000000
+    y=x*(20/100)
+    print(f"Down Payment = Rs.{y}")
