f00c09fefaec7eb6e8a7767e984ba06b144b8054
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2021-12-25 23:16:29 +0530

    Add files via upload

---
+++ Building The Car Game(Teacher's Method).py
@@ -0,0 +1,26 @@
+command=""
+started=False
+while True:
+    command=input("> ").lower()
+    if command=="start":
+        if started:
+            print("Car Is Already Started!...")
+        else:
+            started=True
+            print("Car Started. . . ")
+    elif command=="stop":
+        if not started:
+            print("Car is already Stop")
+        else:
+            started=False
+        print("Car Stopped.")
+    elif command=="help":
+        print("""
+        start - to start the car
+        stop - to stop the car
+        quit - to quit
+        """)
+    elif command=="quit":
+        break
+    else:
+        print("Sorry, I dont Undetstand.")
