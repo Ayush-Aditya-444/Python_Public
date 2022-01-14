320128867ab740e273793509cb43ce050c2be262
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-01-14 18:26:36 +0530

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+print("Dictionaries Example:- ")
+customer1={
+    "name":"Ayush Aditya",
+    "age":20,
+    "gender":"Male",
+    "Country_Citizenship":True
+}
+print(customer1)
+print(customer1["name"])
+print(customer1["Country_Citizenship"])
