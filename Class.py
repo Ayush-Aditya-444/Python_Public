f4e5c71bf4a446271b53873831884c1eff9d36da
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-02-07 19:37:41 +0530

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,11 @@
+class Point:
+    def move(self):
+        print("Ayush")
+    def draw(self):
+        print("Aditya")
+point1=Point()
+point1.move()
+point1.draw()
+point1.x=10
+point1.y=20
+print(point1.x)
