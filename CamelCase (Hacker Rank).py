d2f5ff2445b9262548124ff24e6796aba7e2cb6c
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-05 18:05:56 +0530

    Add files via upload

---
+++ CamelCase (Hacker Rank).py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    a=1
+    for i in range(len(s)):
+        if ord(s[i])>=65 and ord(s[i])<97:
+            a+=1
+            continue
+    return a
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
