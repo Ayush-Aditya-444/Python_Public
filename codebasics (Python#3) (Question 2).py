8a1fbf2c17b5f319f6380376bc9d4f5b57adfa81
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-12 19:36:31 +0530

    Add files via upload

---
+++ codebasics (Python#3) (Question 2).py
@@ -0,0 +1,6 @@
+list1=[299, 298, 300, 303, 330]
+print(list1)
+print("On What Day Price Was 303 Using Linear Searching:-")
+for i in range(len(list1)):
+    if list1[i]==303:
+        print(f"On Day{i} The Price Was 303.")
