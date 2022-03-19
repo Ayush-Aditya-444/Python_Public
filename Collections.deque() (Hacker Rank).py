a09a125e7f585c14cc371f51ece9780296b389d2
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-19 14:25:07 +0530

    Add files via upload

---
+++ Collections.deque() (Hacker Rank).py
@@ -0,0 +1,14 @@
+from collections import deque
+d=deque()
+N=int(input())
+for i in range(N):
+    A=list(input().split())
+    if A[0]=='append':
+        d.append(int(A[1]))
+    elif A[0]=='appendleft':
+        d.appendleft(int(A[1]))
+    elif A[0]=='pop':
+        d.pop()
+    elif A[0]=='popleft':
+        d.popleft()
+print(*d)
