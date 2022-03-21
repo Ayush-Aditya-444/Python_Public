6e4d48a5b27d0902e6d5d6b072214a2734eda05d
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-21 10:26:34 +0530

    Add files via upload

---
+++ CARRANGE (CodeChef).py
@@ -0,0 +1,9 @@
+number_of_input=int(input())
+list1=[]
+for i in range(number_of_input):
+    list2=list(map(int, input().split()))
+    a=(list2[1]-list2[0]+1)*list2[2]
+    list1.append(a)
+    continue
+for i in range(len(list1)):
+    print(list1[i])
