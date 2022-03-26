432280f59e2b275fb32444a3ae9570be36303811
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-03-26 17:34:07 +0530

    Add files via upload

---
+++ FACEDIR (Code Chef).py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    a=int(input())
+    if a%4==0:
+        print("North")
+    elif a%4==1:
+        print("East")
+    elif a%4==2:
+        print("South")
+    elif a%4==3:
+        print("West")
