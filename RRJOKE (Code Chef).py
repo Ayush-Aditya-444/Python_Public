8519a19a79f56878723dd38b6deaff2d57e2bf8d
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-03 09:31:30 +0530

    Add files via upload

---
+++ RRJOKE (Code Chef).py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    a = int(input())
+    temp = 0
+    for i in range(1,a+1):
+        temp ^= i
+    for j in range(a):
+        b = input()
+    print(temp)
