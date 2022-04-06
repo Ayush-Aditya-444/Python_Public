6af7176f238f056152912eb240991208b147d977
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-06 18:20:05 +0530

    Add files via upload

---
+++ CHEFPAGES (Code Chef).py
@@ -0,0 +1,7 @@
+a,b=map(int, input().split())
+if a>=1 and b==0:
+    print("https://www.codechef.com/contests")
+elif a==0 and b>=0:
+    print("https://www.codechef.com/practice")
+else:
+    print("https://discuss.codechef.com")
