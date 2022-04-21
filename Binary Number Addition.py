95bad96136675f847c1a8c8dd8dd0de1d26cdfb3
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-04-21 11:44:08 +0530

    Add files via upload

--- Binary Number Addition.py
+++ Binary Number Addition.py
@@ -1,12 +1,3 @@
-# a=str(input())
-# b=str(input())
-# sum=bin(int(a, 2) + int(b, 2))
-# print(sum[2:])
-def Binary(d):
-   if d>=1:
-       Binary(d// 2)
-   print(d%2, end = '')  
-  
 a=str(input())
 b=str(input())
 j=len(a)-1
@@ -18,12 +9,24 @@ for i in range(len(a)):
         z=z+pow(2,j)
         j=j-1
     else:
+        j=j-1
         continue
+print("z value",z)
 for l in range(len(b)):
     if b[l]=="1":
-        y=y+pow(2,j)
+        y=y+pow(2,k)
         k=k-1
     else:
+        k=k-1
         continue
+print("y value",y)
 h=z+y
-Binary(h)
+print("Sum",h)
+list1=[]
+while h>0:
+    m=h%2
+    m=str(m)
+    list1.append(m)
+    h//=2
+list1.reverse()
+print("".join(list1))
