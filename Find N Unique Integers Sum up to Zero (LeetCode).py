f8899ca749e80b09aa94a581ddef8c571b9bdc0e
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-05-16 00:25:52 +0530

    Add files via upload

---
+++ Find N Unique Integers Sum up to Zero (LeetCode).py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        list1=[]
+        if n%2==0:
+            for i in range(1,n//2+1):
+                list1.append(i)
+                list1.append(-i)
+            return list1
+        else:
+            list1=[0]
+            for i in range(1,n//2+1):
+                list1.append(i)
+                list1.append(-i)
+            return list1
+            
