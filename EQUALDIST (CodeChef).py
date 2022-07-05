abec545a0024eaaf9f98d0290b372f7d8dac8569
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-07-05 16:45:44 +0530

    Add files via upload

---
+++ EQUALDIST (CodeChef).py
@@ -0,0 +1,8 @@
+# cook your dish here
+# cook your dish here
+for i in range(int(input())):
+    a,b=list(map(int, input().split()))
+    if (a+b)%2==0:
+        print("YES")
+    else:
+        print("NO")
