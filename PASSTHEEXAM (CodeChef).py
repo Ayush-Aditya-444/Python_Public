fc9a02b5b8bfc272a3a1f43eb1ce5322a0b91267
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-07-18 16:40:37 +0530

    Add files via upload

---
+++ PASSTHEEXAM (CodeChef).py
@@ -0,0 +1,8 @@
+# cook your dish here
+# cook your dish here
+for i in range(int(input())):
+    a,b,c=map(int, input().split())
+    if a>=10 and b>=10 and c>=10 and a+b+c>=100:
+        print("PASS")
+    else:
+        print("FAIL")
