2a31f24495e1b25b8cec17e7c09cfa92c0d7c117
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-08-21 00:42:41 +0530

    Add files via upload

---
+++ Permutations II (LeetCode).py
@@ -0,0 +1,18 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def ayush(nums):
+            if len(nums)==1:
+                return [nums[:]]
+            list1=[]
+            for i in range(len(nums)):
+                int1=nums[i]
+                list2=nums[:i]+nums[i+1:]
+                for j in ayush(list2):
+                    list1.append([int1]+j)
+            return list1
+        a=ayush(nums)
+        z=[]
+        for i in range(len(a)):
+            if tuple(a[i]) not in z:
+                z.append(tuple(a[i]))
+        return z
