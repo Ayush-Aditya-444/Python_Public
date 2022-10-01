3f7e0b197ce369daf7f2ad61294355203e4cd456
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-10-02 05:02:55 +0530

    Add files via upload

---
+++ Find factorial of a large number (Coding Question).py
@@ -0,0 +1,9 @@
+#User function Template for python3
+
+class Solution:
+    def factorial(self, N):
+        #code here
+        int1=1
+        for i in range(2,N+1):
+            int1=int1*i
+        return [int1]
