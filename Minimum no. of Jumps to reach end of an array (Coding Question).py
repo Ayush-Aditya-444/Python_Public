79359c0fcb5a8e73575976a57cde33015fa8b155
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-10-02 05:00:46 +0530

    Add files via upload

--- Minimum no. of Jumps to reach end of an array (Coding Question).py
+++ Minimum no. of Jumps to reach end of an array (Coding Question).py
@@ -1,6 +1,22 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        dict1=Counter(nums)
-        for i,j in dict1.items():
-            if j>=2:
-                return i
+def ayush(arr):    
+    count1=0
+    int1=len(arr)-1
+    p=0
+    for i in range(len(arr)-1,-1,-1):
+        if arr[i]==0 and i==len(arr)-1:
+            p=1
+            continue
+        if arr[i]==0 and arr[i+1]==0 and p==1:
+            continue 
+        if arr[i]==0:
+            return -1
+        if arr[i]+i>=int1:
+            p=0
+            continue
+        elif arr[i]+i<len(arr):
+            p=0
+            count1+=1
+            int1=i
+    return count1+1
+arr=[1, 3, 5, 8, 0, 2, 6, 7, 6, 8, 9]
+print(ayush(arr))
