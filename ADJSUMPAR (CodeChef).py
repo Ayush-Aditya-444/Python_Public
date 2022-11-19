829ab32fa2aa069de498556c4a80601a816e2cf4
Ayush Aditya <62843082+Ayush-Aditya-444@users.noreply.github.com>
2022-11-19 19:47:18 +0530

    Add files via upload

---
+++ ADJSUMPAR (CodeChef).py
@@ -0,0 +1,10 @@
+# cook your dish here
+for _ in range(int(input())):
+    int1=int(input())
+    list1=list(map(int, input().split()))
+    if ( sum(list1)%2 == 0 ):
+        print("YES")
+    else:
+        print("NO")
+    
+    
